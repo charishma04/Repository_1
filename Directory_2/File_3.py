eb4e2fc403f2ecb1119b072431c60d185635fc6b
charishma04 <67534556+charishma04@users.noreply.github.com>
2020-06-28 13:51:22 +0530

    Update File_3.py

--- Directory_2/File_3.py
+++ Directory_2/File_3.py
@@ -1,10 +1,19 @@
-# Python Program to calculate the square root
 
-# Note: change this value for a different result
-num = 8 
 
-# To take the input from the user
-#num = float(input('Enter a number: '))
+# Python Program to find the area of triangle
 
-num_sqrt = num ** 0.5
-print('The square root of %0.3f is %0.3f'%(num ,num_sqrt))
+a = 5
+b = 6
+c = 7
+
+# Uncomment below to take inputs from the user
+# a = float(input('Enter first side: '))
+# b = float(input('Enter second side: '))
+# c = float(input('Enter third side: '))
+
+# calculate the semi-perimeter
+s = (a + b + c) / 2
+
+# calculate the area
+area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
+print('The area of the triangle is %0.2f' %area)
